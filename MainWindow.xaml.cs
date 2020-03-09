4c40601f7efad7fbc04ba970616134c342e3f416
greg3d <greg3d@mail.ru>
2020-03-10 00:41:07 +0400

    wave

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Win32;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NAudio.Wave;
 
 namespace EarTrainer
 {
@@ -20,9 +22,62 @@ namespace EarTrainer
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "Wave файлы (*.wav)|*.wav|MP3 файлы (*.mp3)|*.mp3";
+            //ofd.FilterIndex = 2;
+            //ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == true)
+            {
+                var inPath = ofd.FileName;
+
+
+
+                if (outputDevice == null)
+                {
+                    outputDevice = new WaveOutEvent();
+                    outputDevice.PlaybackStopped += OnPlayBackStopped;
+                }
+
+                if (audioFile == null)
+                {
+                    audioFile = new AudioFileReader(inPath);
+
+                    audioFile.ToMono();
+                    audioFile.ToStereo();
+
+                    MessageBox.Show(audioFile.WaveFormat.SampleRate.ToString());
+
+
+                    outputDevice.Init(audioFile);
+                }
+                
+
+
+                outputDevice.Play();
+
+                
+
+            }
+        }
+
+        private void OnPlayBackStopped(object sender, StoppedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
